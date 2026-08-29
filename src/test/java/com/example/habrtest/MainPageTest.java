03fe398bc0f5b2605839824d2183382cc5151598
gayterman09-code <gayterman09@gmail.com>
2026-08-29 11:18:34 +0300

    Initial commit

---
+++ src/test/java/com/example/habrtest/MainPageTest.java
@@ -0,0 +1,47 @@
+package com.example.habrtest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://www.github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.habr.com/");
+    }
+
+    @AfterEach
+    public void tearDown() {driver.quit();}
+
+    @Test
+    public void changeLogTest() {
+
+        WebElement userIcon = driver.findElement(By.cssSelector("#app > div > div.tm-footer-menu > div > div > div:nth-child(3) > div > ul > li:nth-child(1) > a"));
+        userIcon.click();
+
+        WebElement rulesLink = driver.findElement(By.xpath("//*[contains(text(), 'Правила сайта')]"));
+        rulesLink.click();
+
+        assertTrue(driver.findElement(By.xpath("//*[@id='app']/div/div[3]/main/div/div/div/div[1]/div/div[2]/div[1]/div[2]/div/span[5]/a")).isDisplayed(),  "Changelog не найден");
+
+    }
+}
