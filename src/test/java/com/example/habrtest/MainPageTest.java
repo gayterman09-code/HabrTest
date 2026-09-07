4c798ba3534ef9a68aae760cd89395ece97e2303
gayterman09-code <gayterman09@gmail.com>
2026-09-07 22:34:13 +0300

    Add visibleAfter

--- src/test/java/com/example/habrtest/MainPageTest.java
+++ src/test/java/com/example/habrtest/MainPageTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -27,37 +29,17 @@ public class MainPageTest {
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.habr.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
     }
 
     @AfterEach
     public void tearDown() {driver.quit();}
 
     @Test
-    public void avtorTest() {
-
-        WebElement userIcon = driver.findElement(By.cssSelector("#app > div > div.tm-footer-menu > div > div > div:nth-child(3) > div > ul > li:nth-child(1) > a"));
-        userIcon.click();
-
-        WebElement tablink = driver.findElement(By.xpath("//*[@id='app']/div/header/div[2]/div/a[contains(text(), 'Все потоки')]"));
-                tablink.click();
-
-        assertTrue(driver.findElement(By.xpath("//*[@id='app']/div/div[3]/main/div/div/div/div[1]/div/div[2]/div[1]/div[2]/div/span[5]/a")).isDisplayed(),  "Авторы не найден");
-
-    }
-
-
-    @Test
-
-    public void companyTest() {
-        WebElement userIcon = driver.findElement(By.cssSelector("#app > div > div.tm-footer-menu > div > div > div:nth-child(3) > div > ul > li:nth-child(1) > a"));
-        userIcon.click();
-
-        WebElement tablink = driver.findElement(By.xpath("//*[@id='app']/div/header/div[2]/div/a[contains(text(), 'Все потоки')]"));
-        tablink.click();
-
-        assertTrue(driver.findElement(By.xpath("//*[@id='app']/div/div[3]/main/div/div/div/div[1]/div/div[2]/div[1]/div[2]/div/span[5]/a")).isDisplayed(),  "Компании не найден");
-
+    public void visibleAfter() {
+        driver.get("https://demoqa.com/dynamic-properties");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        WebElement visibleButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#visibleAfter")));
+        assertTrue(visibleButton.isDisplayed(), "Кнопка не появилась");
     }
 }
